b8a7dd265a5da154f28bc627da6a5cda07566016
tiagopazhs <tiagopaz.gd@gmail.com>
2024-05-14 21:42:07 -0300

    feat: Add pig route

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log('Hello, world!');
+import type { Request, Response } from 'express';
+import express from 'express';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.get('/ping', (_req: Request, res: Response) => {
+    return res.send('pong 🏓')
+})
+
+const PORT = process.env.PORT || 8022;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
