1209f35ca4757c8e4bf86bdf0f10dde9bc5a23a9
tiagopazhs <tiagopaz.gd@gmail.com>
2024-05-15 00:18:29 -0300

    feat: Create migration to create TaskTable

---
+++ src/database/migrations/20240515031315-create-task-table.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('TaskTable', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      actionId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'ActionTable',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('TaskTable');
+  }
+};
