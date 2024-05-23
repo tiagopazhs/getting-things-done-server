09529a2dac8968df72e744518851039568893927
tiagopazhs <tiagopaz.gd@gmail.com>
2024-05-23 08:47:17 -0300

    feat: Create Action Controller Crud operations

--- src/controllers/actions.controller.ts
+++ src/controllers/actions.controller.ts
@@ -18,22 +18,61 @@ class ActionsController {
 
     // Read all actions
     async getAll(req: Request, res: Response): Promise<Response> {
-        return res.status(200).json({ message: 'to do' });
+        try {
+            const actions = await ActionModel.findAll();
+            return res.status(200).json(actions);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: "Failed to get actions", error: error });
+        }
     }
 
     // Read action by ID
     async getById(req: Request, res: Response): Promise<Response> {
-        return res.status(200).json({ message: 'to do' });
+        try {
+            const { id } = req.params;
+            const action = await ActionModel.findByPk(id);
+            if (!action) {
+                return res.status(404).json({ error: "Action not found" });
+            }
+            return res.status(200).json(action);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: "Failed to get action by id", error: error });
+        }
     }
 
     // Update action by ID
     async update(req: Request, res: Response): Promise<Response> {
-        return res.status(200).json({ message: 'to do' });
+        try {
+            const { id } = req.params;
+            const [updatedAction] = await ActionModel.update(req.body, {
+                where: { id },
+            });
+            if (!updatedAction) {
+                return res.status(404).json({ error: "Action not found" });
+            }
+            const updatedData = await ActionModel.findByPk(id);
+            return res.status(200).json(updatedData);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: "Failed to update action", error: error });
+        }
     }
 
     // Delete action by ID
     async delete(req: Request, res: Response): Promise<Response> {
-        return res.status(200).json({ message: 'to do' });
+        try {
+            const { id } = req.params;
+            const deletedAction = await ActionModel.destroy({ where: { id } });
+            if (!deletedAction) {
+                return res.status(404).json({ message: "Action not found" });
+            }
+            return res.status(200).json({ message: "Action deleted with success" });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: "Failed to delete action", error: error });
+        }
     }
 }
 
