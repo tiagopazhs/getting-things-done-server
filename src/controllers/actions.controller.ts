babccfadc01230339d29adbe9075f5a297ac6980
tiagopazhs <tiagopaz.gd@gmail.com>
2024-05-23 08:41:56 -0300

    feat: Create operation at Action controller

--- src/controllers/actions.controller.ts
+++ src/controllers/actions.controller.ts
@@ -5,8 +5,15 @@ class ActionsController {
 
     // CRUD Operations
     // Create a new action
-    async create(req: Request, res: Response): Promise<Response> {
-        return res.status(200).json({ message: 'to do' });
+    async create(req: Request, res: Response) {
+        try {
+            const { name, description, initialDate } = req.body;
+            const createdAction = await ActionModel.create({ name, description, initialDate, status: 'Open' });
+            res.status(201).json(createdAction);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Failed to create action', error: error });
+        }
     }
 
     // Read all actions
